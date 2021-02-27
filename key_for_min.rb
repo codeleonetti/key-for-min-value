7586af4842a137dbe4a0a6d90ea65422907d5b45
john <john.d.leonetti@gmail.com>
2021-02-27 12:35:12 -0800

    commit tree 0e724df4ee4d632d42513a7ad531f8757c406f24

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    result_key = nil
+    result_value = 600
+    name_hash.each do |key, value|
+        if result_value >= value 
+             result_value = value
+            result_key = key   
+        end
+    end  
+        return result_key
 end
